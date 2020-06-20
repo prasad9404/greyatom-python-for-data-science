91389570305c79225e784fa624572c107f0be8c9
Prasad Chaudhari <chaudhariprasad1999@gmail.com>
2020-06-20 23:32:13 +0530

    Update code.py

--- -Make-Sense-of-Census/code.py
+++ -Make-Sense-of-Census/code.py
@@ -1,82 +1,99 @@
-# --------------
 # Importing header files
 import numpy as np
-import warnings
-
-warnings.filterwarnings('ignore')
 
 #New record
 new_record=[[50,  9,  4,  1,  0,  0, 40,  0]]
 
-#Reading file
+#Code starts here
+
+#Loading data file and saving it into a new numpy array 
 data = np.genfromtxt(path, delimiter=",", skip_header=1)
 
-#Code starts here
-census=np.concatenate((data,new_record),axis=0)
+print(data.shape)
 
-age = np.array(census[:,0])
+#Concatenating the new record to the existing numpy array
+census=np.concatenate((data, new_record),axis = 0)
 
-max_age = np.max(age)
-min_age = np.min(age)
-age_mean= np.mean(age)
-age_std= np.std(age,axis=None)
+print(census.shape)
 
-race_0=[]
-race_1=[]
-race_2=[]
-race_3=[]
-race_4=[]
+#Subsetting the array to include only 'Age' column
+age=census[:,0]
 
-for row in census:
-    if row[2] == 0.0:
-        race_0.append(row)
-    elif row[2] == 1.0:
-        race_1.append(row)
-    elif row[2] == 2.0:
-        race_2.append(row)
-    elif row[2] == 3.0:
-        race_3.append(row)
-    elif row[2] == 4.0:
-        race_4.append(row)
+#Finding the max value of age
+max_age=age.max()
+print("Max Age= ",max_age)
+print(age)
 
-race_0 = np.array(race_0)
-race_1 = np.array(race_1)
-race_2 = np.array(race_2)
-race_3 = np.array(race_3)
-race_4 = np.array(race_4)
-#print(race_0)
-len_0 = len(race_0)
-len_1 = len(race_1)
-len_2 = len(race_2)
-len_3 = len(race_3)
-len_4 = len(race_4)
-#print(len_0,len_1,len_2,len_3,len_4)
-temp=np.array([len_0,len_1,len_2,len_3,len_4])
-minimum = temp.min()
-minority_race = np.where(temp == minimum)
-#print(minority_race[0][0])
-filterarr= census[:,0]>60
-senior_citizens = census[filterarr]
+#Find the min value of age
+min_age=age.min()
+print("Min Age= ",min_age)
 
-working_hours_sums=0
+#Find the mean of age
+age_mean=age.mean()
+print("Age Average= ", age_mean)
 
-for row in senior_citizens:
-    working_hours_sums += row[6]
+#Find the standard deviation of age
+age_std=age.std()
+print("Age Standard Deviation= ",age_std)
 
-senior_citizens_len = len(senior_citizens)
-avg_working_hours = working_hours_sums / senior_citizens_len
-
-print(round(avg_working_hours,2))
-
-greater = census[:,1] > 10
-lower = census[:,1] <= 10
-
-high = census[greater]
-low = census[lower]
-
-avg_pay_high = round(np.mean(high[:,7]),2)
-avg_pay_low = round(np.mean(low[:,7]),2)
-
-print(avg_pay_high,avg_pay_low)
+#Creating new subsets based on 'Age'
+race_0=census[census[:,2]==0]
+race_1=census[census[:,2]==1]
+race_2=census[census[:,2]==2]
+race_3=census[census[:,2]==3]
+race_4=census[census[:,2]==4]
 
 
+#Finding the length of the above created subsets
+len_0=len(race_0)
+len_1=len(race_1)
+len_2=len(race_2)
+len_3=len(race_3)
+len_4=len(race_4)
+
+#Printing the length of the above created subsets
+print('Race_0: ', len_0)
+print('Race_1: ', len_1)
+print('Race_2: ', len_2)
+print('Race_3: ', len_3)
+print('Race_4: ', len_4)
+
+#Storing the different race lengths with appropriate indexes
+race_list=[len_0, len_1,len_2, len_3, len_4]
+
+#Storing the race with minimum length into a variable 
+minority_race=race_list.index(min(race_list))
+
+#Subsetting the array based on the age 
+senior_citizens=census[census[:,0]>60]
+
+#Calculating the sum of all the values of array
+working_hours_sum=senior_citizens.sum(axis=0)[6]
+
+#Finding the length of the array
+senior_citizens_len=len(senior_citizens)
+
+#Finding the average working hours
+avg_working_hours=working_hours_sum/senior_citizens_len
+
+#Printing the average working hours
+print((avg_working_hours))
+
+#Creating an array based on 'education' column
+high=census[census[:,1]>10]
+
+#Finding the average pay
+avg_pay_high=high[:,7].mean()
+
+#Printing the average pay
+print(avg_pay_high)
+
+#Creating an array based on 'education' column
+low=census[census[:,1]<=10]
+
+#Finding the average pay
+avg_pay_low=low[:,7].mean()
+
+#Printing the average pay
+print(avg_pay_low)
+#Code ends here
